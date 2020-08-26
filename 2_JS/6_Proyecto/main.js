0c19da22481f07ba4de1f775210d3fc821c99db3
ulmae <68133338+ulmae@users.noreply.github.com>
2020-08-25 21:14:23 -0600

    Archivos 8/25/2020

---
+++ 2_JS/6_Proyecto/main.js
@@ -0,0 +1,5 @@
+var cuentas = [
+    { nombre: "Mali", saldo: 200, password: 'helloworld' },
+    { nombre: "Gera", saldo: 290, password: 'l33t' },
+    { nombre: "Maui", saldo: 67, password: '123' }
+];
