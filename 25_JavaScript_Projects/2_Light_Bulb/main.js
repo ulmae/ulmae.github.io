f70f51430f45acfb47fea0288652c9d60b73f657
Ulma Escobar <ulmae18@gmail.com>
2023-03-08 00:48:16 -0600

    Light bulb

---
+++ 25_JavaScript_Projects/2_Light_Bulb/main.js
@@ -0,0 +1,35 @@
+var lightbulb = document.getElementById("lightbulb");
+var button = document.getElementById("switch");
+var currentColor;
+currentColor = "#FFE900";
+
+var lightsOn = true;
+
+const colorPicker = document.getElementById("light-color");
+colorPicker.addEventListener("change", (event) => {
+    if (lightsOn === true) {
+        currentColor = colorPicker.value;
+        console.log(currentColor);
+        lightbulb.style.backgroundColor = currentColor;
+        lightbulb.style.boxShadow = `0px 0px 100px ${currentColor}`;
+    } else {
+        currentColor = colorPicker.value;
+        console.log(currentColor);
+    }
+
+});
+
+function activateSwitch() {
+    lightsOn = !lightsOn;
+    console.log(lightsOn)
+    if (lightsOn === true) {
+        lightbulb.style.backgroundColor = currentColor;
+        lightbulb.style.boxShadow = `0px 0px 100px ${currentColor}`;
+        button.innerText = "Switch Off"
+        
+    } else {
+        lightbulb.style.backgroundColor = '#000000';
+        lightbulb.style.boxShadow = `0px 0px 100px #000000`;
+        button.innerText = "Switch On"
+    }
+}
