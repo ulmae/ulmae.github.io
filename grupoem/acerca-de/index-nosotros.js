2f83b10e7d5b25c97902079c02c72d50c7966b10
Ulma Escobar <ulmae18@gmail.com>
2024-04-14 14:10:01 -0600

    GrupoEM

---
+++ grupoem/acerca-de/index-nosotros.js
@@ -0,0 +1,7 @@
+const navToggle = document.querySelector(".nav-toggle")
+const navMenu = document.querySelector(".nav-menu")
+
+navToggle.addEventListener("click", () => {
+    navMenu.classList.toggle("nav-menu-visible")
+}
+)
